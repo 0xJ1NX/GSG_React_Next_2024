f40766ea4f2be7e1480b3903fbf3ab6aff82d696
Omar <omarqalaweh@gmail.com>
2025-01-10 23:55:08 +0200

    todo list app upload

--- HW2 (TodoList)/src/components/form/form.component.tsx
+++ HW2 (TodoList)/src/components/form/form.component.tsx
@@ -2,38 +2,37 @@ import React from "react";
 import { ITodoItem } from "../../types";
 import './form.css';
 
-
 interface IProps {
-    onSubmit: (item: ITodoItem) => void;
-  }
-  
-  const Form = (props: IProps) => {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const title: string = e.currentTarget["task"].value;
-      const isUrgent: boolean = e.currentTarget["urgent"].checked;
-      if (title.length > 3) {
-        const newTask: ITodoItem = {
-          id: Date.now(),
-          title,
-          isUrgent,
-          isDone: false
-        }
-  
-        props.onSubmit(newTask);
+  onSubmit: (item: ITodoItem) => void;
+}
+
+const Form = (props: IProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const title: string = e.currentTarget["task"].value;
+    const isUrgent: boolean = e.currentTarget["urgent"].checked;
+    if (title.length > 3) {
+      const newTask: ITodoItem = {
+        id: Date.now(),
+        title,
+        isUrgent,
+        isDone: false
       }
+
+      props.onSubmit(newTask);
     }
-  
-    return (
-      <form className="form-wrapper" onSubmit={handleSubmit}>
-        <input type="text" name="task" placeholder="Type todo here..." />
-        <div>
-          <label htmlFor="urgent">Urgent</label>
-          <input type="checkbox" id="urgent" name="urgent" />
-        </div>
-        <input type="submit" value="Add Todo" />
-      </form>
-    )
   }
-  
-  export default Form
+
+  return (
+    <form className="form-wrapper" onSubmit={handleSubmit}>
+      <input className="task-input" type="text" name="task" placeholder="Type todo here..." />
+      <div>
+        <input type="checkbox" id="urgent" name="urgent" />
+        <label htmlFor="urgent">Urgent</label>
+      </div>
+      <input className="submit" type="submit" value="Add Todo" />
+    </form>
+  )
+}
+
+export default Form
